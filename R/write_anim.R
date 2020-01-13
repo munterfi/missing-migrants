986e8b4d89e3edc2bda12fba0980078b21424c62
munterfinger <lyn@hispeed.ch>
2020-01-13 07:08:48 +0100

    Add weight to geographic centroid, number of incidents

--- R/write_anim.R
+++ R/write_anim.R
@@ -2,5 +2,5 @@ write_anim <- function(gplot, anim_file_name) {
   require(gganimate)
   anim_file_name %>%
     sprintf(mig$year %>% min(), mig$year %>% max()) %>% 
-    anim_save(gplot, width = 1600, height = 900)
+    anim_save(gplot, nframes = 150, width = 1600, height = 900)
 }
